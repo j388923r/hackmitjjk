043fe00a6ccd8f6f146a90261f75c8d1a06a7b0d
Jamar Brooks <j388923r@gmail.com>
2015-09-19 13:44:02 -0400

    add angular and semantic

---
+++ public/javascripts/app.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules
+        'ngAnimate',
+        'ngRoute'
+
+        // Custom modules
+
+        // 3rd Party Modules
+    ]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/", {
+            controller: 'homeController',
+            templateUrl: '/views/home.html'
+        });
+    });
+})();
