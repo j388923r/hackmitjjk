caf64f1d61ad6371d6a5ba93ae45ea5d42c7ab94
Katharine Xiao <kjx@mit.edu>
2015-09-20 01:04:52 -0400

    addfa

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,27 @@ var fbgraph = require('fbgraphapi');
 //   res.render('index', { title: 'Express' });
 // });
 router.get('/', function(req, res) {
-    if (!req.hasOwnProperty('facebook')) {
-        console.log('You are not logged in');
-        return res.redirect('/login');
-    }
-    /* See http://developers.facebook.com/docs/reference/api/ for more */
-    req.facebook.graph('/me', function(err, me) {
-        console.log(me);
-    });
+    // if (!req.hasOwnProperty('facebook')) {
+    //     console.log('You are not logged in');
+    //     return res.redirect('/login');
+    // }
+    // /* See http://developers.facebook.com/docs/reference/api/ for more */
+    // req.facebook.graph('/me', function(err, me) {
+    //     console.log(me);
+    // });
     
-    req.facebook.graph('/me?fields=id,name', function(err, me) {
-        console.log(me);
-    });
+    // req.facebook.graph('/me?fields=id,name', function(err, me) {
+    //     console.log(me);
+    // });
     
-    req.facebook.me(function(err, me) {
-        console.log(me);
-    });
+    // req.facebook.me(function(err, me) {
+    //     console.log(me);
+    // });
     
-    // /me/likes
-    req.facebook.my.likes(function(err, likes) {
-        console.log(likes);
-    });
+    // // /me/likes
+    // req.facebook.my.likes(function(err, likes) {
+    //     console.log(likes);
+    // });
     
     // res.end("Check console output");
     res.render('index', { title: 'Express' });
