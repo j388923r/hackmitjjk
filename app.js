6cca540e58e853fff74faea41181c11a37803c9b
Jamar Brooks <j388923r@gmail.com>
2015-09-20 03:26:21 -0400

    made routing relative

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    callbackURL: "/auth/facebook/callback",
     enableProof: false
   },
   function(accessToken, refreshToken, profile, done) {
